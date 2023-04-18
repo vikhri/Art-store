71875678428f2c17003525d67431cba39a4e631e
ira <vikhrova.irina@gmail.com>
2023-04-18 13:11:27 +0600

    webpack config without images

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import './main.scss';
+
+
+function component(text) {
+    const element = document.createElement('h1');
+    element.textContent = text;
+    return element;
+}
+
+document.body.prepend(component('Проект собран на Webpack'));
